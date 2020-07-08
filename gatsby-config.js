ab0f538e73ae965a0d88a81ab88c61126783b776
jiansing <xkmhg@outlook.com>
2020-07-08 23:24:50 +0800

    add

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,33 @@ module.exports = {
 		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-sass',
 		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+			  name: `网站任意门`,
+			  short_name: `网站任意门`,
+			  description: `开启一次未知之旅，这里总有一个属于你的宝站！`,
+			  start_url: `/`,
+			  background_color: `#fff`,
+			  theme_color: `#ea997c`,
+			  display: `standalone`,
+			  icons: [
+				{
+				  src: `/android-chrome-192x192.png`,
+				  sizes: `192x192`,
+				  type: `image/png`,
+				},
+				{
+				  src: `/android-chrome-512x512.png`,
+				  sizes: `512x512`,
+				  type: `image/png`,
+				},
+			  ],
+			  legacy: false, // this will not add apple-touch-icon links to <head>
+			  crossOrigin: `use-credentials`, // `use-credentials` or `anonymous`
+	  
+			},
+		  },
+		{
 			resolve: "gatsby-plugin-google-tagmanager",
 			options: {
 			  id: "GTM-NFCB2V4",
